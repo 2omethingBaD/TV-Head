ab18be15c095fdf78f159781ee82380a67689604
2omethingBaD <113650499+2omethingBaD@users.noreply.github.com>
2023-08-11 17:49:40 -0400

    added double button push animations/ code cleanup

--- TVhead.ino
+++ TVhead.ino
@@ -20,7 +20,7 @@ EasyButton button_two(button_two_pin);
 EasyButton button_three(button_three_pin);
 EasyButton button_four(button_four_pin);
 EasyButton button_five(button_five_pin);
-uint8_t pattern_counter = 1;
+uint8_t pattern_counter = 11;
 
 
 //eye open
@@ -71,6 +71,23 @@ const long ledarray6[] PROGMEM = {
 				0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 
 				0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 
 				0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 
+				0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 
+				0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 
+};
+//X eye
+const long ledarray7[] PROGMEM = {
+				0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 
+				0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 
+				0x00FF6FFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FF6FFF, 
+				0x00FF6FFF, 0x00FF6FFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FF6FFF, 0x00FF6FFF, 
+				0x00000000, 0x00FF6FFF, 0x00FF6FFF, 0x00000000, 0x00000000, 0x00000000, 0x00FF6FFF, 0x00FF6FFF, 0x00000000, 
+				0x00000000, 0x00000000, 0x00FF6FFF, 0x00FF6FFF, 0x00000000, 0x00FF6FFF, 0x00FF6FFF, 0x00000000, 0x00000000, 
+				0x00000000, 0x00000000, 0x00000000, 0x00FF6FFF, 0x00FF6FFF, 0x00FF6FFF, 0x00000000, 0x00000000, 0x00000000, 
+				0x00000000, 0x00000000, 0x00000000, 0x00FF6FFF, 0x00FF6FFF, 0x00FF6FFF, 0x00000000, 0x00000000, 0x00000000, 
+				0x00000000, 0x00000000, 0x00FF6FFF, 0x00FF6FFF, 0x00000000, 0x00FF6FFF, 0x00FF6FFF, 0x00000000, 0x00000000, 
+				0x00000000, 0x00FF6FFF, 0x00FF6FFF, 0x00000000, 0x00000000, 0x00000000, 0x00FF6FFF, 0x00FF6FFF, 0x00000000, 
+				0x00FF6FFF, 0x00FF6FFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FF6FFF, 0x00FF6FFF, 
+				0x00FF6FFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FF6FFF, 
 				0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 
 				0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 
 };
@@ -149,9 +166,17 @@ void onB1Press()
 {
   pattern_counter = 1;
 }
+void onB1DPress()
+{
+  pattern_counter = 11;
+}
 void onB2Press()
 {
   pattern_counter = 2;
+}
+void onB2DPress()
+{
+  pattern_counter = 22;
 }
 void onB3Press()
 {
@@ -175,7 +200,9 @@ void setup(){
   button_one.begin();
   button_two.begin();
   button_one.onPressed(onB1Press);
+  button_one.onSequence(2, 2000, onB1DPress);
   button_two.onPressed(onB2Press);
+  button_two.onSequence(2, 2000, onB2DPress);
   button_three.onPressed(onB3Press);
   button_four.onPressed(onB4Press);
   button_five.onPressed(onB5Press);
@@ -191,290 +218,345 @@ void loop(){
 
   switch (pattern_counter) {
     case 1:
+      openEye();
+      break;
+    case 11:
       blink();
       break;
     case 2:
+      squintEye();
+      break;
+    case 22:
       squint();
       break;
     case 3:
-    openEye();
-    break;
+      xEye();
+      break;
     case 4:
-    squintEye();
-    break;
+      UwUEye();
+      break;
     case 5:
-    closedEye();
-    break;
+      closedEye();
+      break;
   }
 }
 
 
 //static open eye
 void openEye(){
-  while(pattern_counter == 3){
+  while(pattern_counter == 1){
     for(int i = 0; i < num_led; i++){
     leds[i] = pgm_read_dword(&(ledarray0[i]));
     button_one.read();
-  button_two.read();
-  button_four.read();
-  button_five.read();
+    button_two.read();
+    button_three.read();
+    button_four.read();
+    button_five.read();
   }
   FastLED.delay(80);
   }
 }
-
-
 //static squinted eye
 void squintEye(){
-  while(pattern_counter == 4){
+  while(pattern_counter == 2){
     for(int i = 0; i < num_led; i++){
     leds[i] = pgm_read_dword(&(ledarray5[i]));
     button_one.read();
-  button_two.read();
-  button_three.read();
-  button_five.read();
+    button_two.read();
+    button_three.read();
+    button_four.read();
+    button_five.read();
   }
   FastLED.delay(80);
   }
 }
-
-
+//static X eye
+void xEye(){
+  while(pattern_counter == 3){
+    for(int i = 0; i < num_led; i++){
+    leds[i] = pgm_read_dword(&(ledarray7[i]));
+    button_one.read();
+    button_two.read();
+    button_three.read();
+    button_four.read();
+    button_five.read();
+  }
+  FastLED.delay(80);
+  }
+}
+//static UwU eye
+void UwUEye(){
+  while(pattern_counter == 4){
+    for(int i = 0; i < num_led; i++){
+    leds[i] = pgm_read_dword(&(ledarray6[i]));
+    button_one.read();
+    button_two.read();
+    button_three.read();
+    button_four.read();
+    button_five.read();
+  }
+  FastLED.delay(80);
+  }
+}
 //static closed eye
 void closedEye(){
   while(pattern_counter == 5){
     for(int i = 0; i < num_led; i++){
-    leds[i] = pgm_read_dword(&(ledarray6[i]));
+    leds[i] = pgm_read_dword(&(ledarray4[i]));
     button_one.read();
-  button_two.read();
-  button_three.read();
-  button_four.read();
+    button_two.read();
+    button_three.read();
+    button_four.read();
+    button_five.read();
   }
   FastLED.delay(80);
   }
 }
-
-
 //idle animation /blink animation
 void blink(){ 
-  while(pattern_counter == 1){
-  button_two.read();
-  button_three.read();
-  button_four.read();
-  button_five.read();
-  for(int i = 0; i < num_led; i++)
-  {
+  while(pattern_counter == 11){
+    button_one.read();
     button_two.read();
     button_three.read();
-  button_four.read();
-  button_five.read();
+    button_four.read();
+    button_five.read();
+  for(int i = 0; i < num_led; i++)
+  {
+    button_one.read();
+    button_two.read();
+    button_three.read();
+    button_four.read();
+    button_five.read();
     leds[i] = pgm_read_dword(&(ledarray0[i]));
   }
   FastLED.delay(6000);
   
   for(int i = 0; i < num_led; i++){
+    button_one.read();
     button_two.read();
     button_three.read();
-  button_four.read();
-  button_five.read();
+    button_four.read();
+    button_five.read();
     leds[i] = pgm_read_dword(&(ledarray1[i]));
   }
   FastLED.delay(80);
  
   for(int i = 0; i < num_led; i++){
+    button_one.read();
     button_two.read();
     button_three.read();
-  button_four.read();
-  button_five.read();
+    button_four.read();
+    button_five.read();
     leds[i] = pgm_read_dword(&(ledarray2[i]));
   }
   FastLED.delay(80);
   
   for(int i = 0; i < num_led; i++){
+    button_one.read();
     button_two.read();
     button_three.read();
-  button_four.read();
-  button_five.read();
+    button_four.read();
+    button_five.read();
     leds[i] = pgm_read_dword(&(ledarray3[i]));
   }
   FastLED.delay(80);
   
   for(int i = 0; i < num_led; i++){
+    button_one.read();
     button_two.read();
     button_three.read();
-  button_four.read();
-  button_five.read();
+    button_four.read();
+    button_five.read();
     leds[i] = pgm_read_dword(&(ledarray4[i]));
   }
   FastLED.delay(60);
 
   for(int i = 0; i < num_led; i++){
+    button_one.read();
     button_two.read();
     button_three.read();
-  button_four.read();
-  button_five.read();
+    button_four.read();
+    button_five.read();
     leds[i] = pgm_read_dword(&(ledarray2[i]));
   }
   FastLED.delay(60);
 
   for(int i = 0; i < num_led; i++){
+    button_one.read();
     button_two.read();
     button_three.read();
-  button_four.read();
-  button_five.read();
+    button_four.read();
+    button_five.read();
     leds[i] = pgm_read_dword(&(ledarray3[i]));
   }
   FastLED.delay(60);
 
   for(int i = 0; i < num_led; i++){
+    button_one.read();
     button_two.read();
     button_three.read();
-  button_four.read();
-  button_five.read();
+    button_four.read();
+    button_five.read();
     leds[i] = pgm_read_dword(&(ledarray4[i]));
   }
   FastLED.delay(80);
 
   for(int i = 0; i < num_led; i++){
+    button_one.read();
     button_two.read();
     button_three.read();
-  button_four.read();
-  button_five.read();
+    button_four.read();
+    button_five.read();
     leds[i] = pgm_read_dword(&(ledarray3[i]));
   }
   FastLED.delay(80);
 
   for(int i = 0; i < num_led; i++){
+    button_one.read();
     button_two.read();
     button_three.read();
-  button_four.read();
-  button_five.read();
+    button_four.read();
+    button_five.read();
     leds[i] = pgm_read_dword(&(ledarray2[i]));
   }
   FastLED.delay(80);
 
   for(int i = 0; i < num_led; i++){
+    button_one.read();
     button_two.read();
     button_three.read();
-  button_four.read();
-  button_five.read();
+    button_four.read();
+    button_five.read();
     leds[i] = pgm_read_dword(&(ledarray1[i]));
   }
   FastLED.delay(80);
+  button_one.read();
   button_two.read();
   button_three.read();
   button_four.read();
   button_five.read();
   }
 }
-
-
 //tired eye blink animation
 void squint(){
-  while(pattern_counter == 2){
-  button_one.read();
-  button_three.read();
-  button_four.read();
-  button_five.read();
+  while(pattern_counter == 22){
+    button_one.read();
+    button_two.read();
+    button_three.read();
+    button_four.read();
+    button_five.read();
   for(int i = 0; i < num_led; i++)
   {
     button_one.read();
+    button_two.read();
     button_three.read();
-  button_four.read();
-  button_five.read();
+    button_four.read();
+    button_five.read();
     leds[i] = pgm_read_dword(&(ledarray5[i]));
   }
   FastLED.delay(6000);
  
   for(int i = 0; i < num_led; i++){
     button_one.read();
+    button_two.read();
     button_three.read();
-  button_four.read();
-  button_five.read();
+    button_four.read();
+    button_five.read();
     leds[i] = pgm_read_dword(&(ledarray1[i]));
   }
   FastLED.delay(80);
   
   for(int i = 0; i < num_led; i++){
     button_one.read();
+    button_two.read();
     button_three.read();
-  button_four.read();
-  button_five.read();
+    button_four.read();
+    button_five.read();
     leds[i] = pgm_read_dword(&(ledarray2[i]));
   }
   FastLED.delay(80);
  
   for(int i = 0; i < num_led; i++){
     button_one.read();
+    button_two.read();
     button_three.read();
-  button_four.read();
-  button_five.read();
+    button_four.read();
+    button_five.read();
     leds[i] = pgm_read_dword(&(ledarray3[i]));
   }
   FastLED.delay(80);
   
   for(int i = 0; i < num_led; i++){
     button_one.read();
+    button_two.read();
     button_three.read();
-  button_four.read();
-  button_five.read();
+    button_four.read();
+    button_five.read();
     leds[i] = pgm_read_dword(&(ledarray4[i]));
   }
   FastLED.delay(60);
 
   for(int i = 0; i < num_led; i++){
     button_one.read();
+    button_two.read();
     button_three.read();
-  button_four.read();
-  button_five.read();
+    button_four.read();
+    button_five.read();
     leds[i] = pgm_read_dword(&(ledarray2[i]));
   }
   FastLED.delay(60);
 
   for(int i = 0; i < num_led; i++){
     button_one.read();
+    button_two.read();
     button_three.read();
-  button_four.read();
-  button_five.read();
+    button_four.read();
+    button_five.read();
     leds[i] = pgm_read_dword(&(ledarray3[i]));
   }
   FastLED.delay(60);
 
   for(int i = 0; i < num_led; i++){
     button_one.read();
+    button_two.read();
     button_three.read();
-  button_four.read();
-  button_five.read();
+    button_four.read();
+    button_five.read();
     leds[i] = pgm_read_dword(&(ledarray4[i]));
   }
   FastLED.delay(80);
 
   for(int i = 0; i < num_led; i++){
     button_one.read();
+    button_two.read();
     button_three.read();
-  button_four.read();
-  button_five.read();
+    button_four.read();
+    button_five.read();
     leds[i] = pgm_read_dword(&(ledarray3[i]));
   }
   FastLED.delay(80);
 
   for(int i = 0; i < num_led; i++){
     button_one.read();
+    button_two.read();
     button_three.read();
-  button_four.read();
-  button_five.read();
+    button_four.read();
+    button_five.read();
     leds[i] = pgm_read_dword(&(ledarray2[i]));
   }
   FastLED.delay(80);
 
   for(int i = 0; i < num_led; i++){
     button_one.read();
+    button_two.read();
     button_three.read();
-  button_four.read();
-  button_five.read();
+    button_four.read();
+    button_five.read();
     leds[i] = pgm_read_dword(&(ledarray1[i]));
   }
   FastLED.delay(80);
   button_one.read();
+  button_two.read();
   button_three.read();
   button_four.read();
   button_five.read();
